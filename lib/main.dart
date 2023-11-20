313d96692e722ca85057b803690788e4c8ad1da1
ahmedkaimkhani <ak9302359@gmail.com>
2023-11-21 01:17:04 +0500

    Api with stacked statemanagement

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:api_with_stacked/app/app.locator.dart';
+import 'package:api_with_stacked/app/app.router.dart';
 import 'package:flutter/material.dart';
-
-import 'views/home_view.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 void main() async {
+  await setupLocator();
   runApp(const MyApp());
 }
 
@@ -12,9 +14,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeView(),
+      navigatorKey: StackedService.navigatorKey,
+      onGenerateRoute: StackedRouter().onGenerateRoute,
     );
   }
 }
